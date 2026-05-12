15989e720e1fd3c345b27b04c1d7360ec63dfe1c
samar-server <samar786h@gmail.com>
2026-05-12 18:37:38 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "API Online"
+    })
+})
+
+app.post("/login", (req, res) => {
+
+    const { username, password } = req.body
+
+    if(username === "admin" && password === "1234"){
+
+        return res.json({
+            success: true,
+            token: "welcome123"
+        })
+    }
+
+    res.json({
+        success: false
+    })
+})
+
+app.listen(3000, "0.0.0.0", () => {
+    console.log("Server Started")
+})
